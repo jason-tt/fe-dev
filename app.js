579acb07499a5b5f0b8deb02c71bee9110177075
Jason Kim <jasonk@thumbtack.com>
2016-10-20 18:07:30 -0700

    Add JS to handle navbar clicks and modal behavior

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 // JS code
 (function() {
-    console.log('hello');
+    $('#get-quotes').on('click', function(e) {
+        e.stopPropagation();
+        console.log('button');
+        $('#modal-bg').removeClass('invisible');
+    });
+
+    $('#modal-bg').on('click', function(e) {
+        e.stopPropagation();
+        if (e.target.id === 'request-form') {
+            return;
+        }
+        $('#modal-bg').addClass('invisible');
+    });
+
+    document.getElementById('nav').onclick = function(e) {
+        e.stopPropagation();
+        if (e.target.tagName === 'A') {
+            console.log('nav');
+        }
+    };
 })();
